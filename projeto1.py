d13099aeec808f57a382ed8f9986f62797f6d7f5
Lucas Xavier <xavier.lucas.original.7@gmail.com>
2025-03-20 15:10:06 -0300

    commit 2

--- projeto1.py
+++ projeto1.py
@@ -19,11 +19,29 @@ def calcular_area(cultura):
 
 
 def cadastrar_plantacao():
-    cultura = input("Digite a cultura (laranja/milho): ").lower()
-    area = calcular_area(cultura)
-
-    plantacoes.append({"cultura": cultura, "area": area})
+    nome = input("Digite o nome da plantação: ")
+    while True:
+        cultura = input("Digite a cultura (laranja/milho): ").lower()
+        if cultura == "laranja" or cultura == "milho":
+            area = calcular_area(cultura)
+            break
+        else:
+            print("\n ! Cultura inválida ! \n")
+    sementes_mudas, agua, nitrogenio, fosforo, potassio = '', '', '', '', ''
+    plantacoes.append({"nome": nome, "cultura": cultura, "area": area,
+                       "sementes/mudas": sementes_mudas, "agua": agua,
+                       "nitrogenio": nitrogenio, "fosforo": fosforo, "potassio": potassio})
     print("Plantação cadastrada com sucesso!")
+
+
+def exibir_plantacoes():
+    if not plantacoes:
+        print("Nenhuma plantação cadastrada.")
+    else:
+        print("\n ---> Plantações: ")
+        for i, plantacao in enumerate(plantacoes):
+            print(
+                f"{i} - Nome: {plantacao['nome'].capitalize()} || Cultura: {plantacao['cultura'].capitalize()} || Área: {plantacao['area']} m²")
 
 
 def atualizar_area():
@@ -61,18 +79,39 @@ def calcular_insumos():
     if 0 <= indice < len(plantacoes):
         cultura = plantacoes[indice]["cultura"]
         area = plantacoes[indice]["area"]
+        ########## Dados coletados do site da EMBRAPA ##########
         if cultura == "milho":
             sementes = 2.3 * area  # Média de 2.3g por m²
+            plantacoes[indice]["sementes/mudas"] = sementes
+
             agua = 5 * area  # Aproximadamente 5 litros por m²
+            plantacoes[indice]["agua"] = agua
+
             nitrogenio = 10 * area  # Média de 10g de N por m²
+            plantacoes[indice]["nitrogenio"] = nitrogenio
+
             fosforo = 5 * area  # Média de 5g de P por m²
+            plantacoes[indice]["fosforo"] = fosforo
+
             potassio = 8 * area  # Média de 8g de K por m²
+            plantacoes[indice]["potassio"] = potassio
+
         elif cultura == "laranja":
             mudas = round(area / 30, 3)  # Média de uma muda a cada 30m²
+            plantacoes[indice]["sementes/mudas"] = mudas
+
             agua = 15 * area  # Aproximadamente 15 litros por m²
+            plantacoes[indice]["agua"] = agua
+
             nitrogenio = 8 * area  # Média de 8g de N por m²
+            plantacoes[indice]["nitrogenio"] = nitrogenio
+
             fosforo = 4 * area  # Média de 4g de P por m²
+            plantacoes[indice]["fosforo"] = fosforo
+
             potassio = 6 * area  # Média de 6g de K por m²
+            plantacoes[indice]["potassio"] = potassio
+
         else:
             print("Cultura inválida!")
             return
@@ -89,15 +128,7 @@ def calcular_insumos():
         print("Índice inválido!")
 
 
-def exibir_plantacoes():
-    if not plantacoes:
-        print("Nenhuma plantação cadastrada.")
-    else:
-        for i, plantacao in enumerate(plantacoes):
-            print(f"{i} - Cultura: {plantacao['cultura'].capitalize()}, Área: {plantacao['area']} m²")
-
-
-def menu():
+def main():
     while True:
         print("\n==== Menu de Opções ====")
         print("1 - Cadastrar plantação")
@@ -122,4 +153,22 @@ def menu():
             print("Opção inválida!")
 
 
-menu()
+########## Plantações exemplo para o programa em R ##########
+plantacoes.append({'nome': 'Plantação laranja 1', 'cultura': 'laranja', 'area': 47505.06, 'sementes/mudas': 1583.502,
+                   'agua': 712575.8999999999, 'nitrogenio': 380040.48, 'fosforo': 190020.24, 'potassio': 285030.36})
+plantacoes.append({'nome': 'Plantação laranja 2', 'cultura': 'laranja', 'area': 652919.04, 'sementes/mudas': 21763.968,
+                   'agua': 9793785.600000001, 'nitrogenio': 5223352.32, 'fosforo': 2611676.16, 'potassio': 3917514.24})
+plantacoes.append({'nome': 'Plantação laranja 3', 'cultura': 'laranja', 'area': 1954715.94, 'sementes/mudas': 65157.198,
+                   'agua': 29320739.099999998, 'nitrogenio': 15637727.52, 'fosforo': 7818863.76,
+                   'potassio': 11728295.64})
+plantacoes.append(
+    {'nome': 'Plantação milho 1', 'cultura': 'milho', 'area': 15129.0, 'sementes/mudas': 34796.7, 'agua': 75645.0,
+     'nitrogenio': 151290.0, 'fosforo': 75645.0, 'potassio': 121032.0})
+plantacoes.append(
+    {'nome': 'Plantação milho 2', 'cultura': 'milho', 'area': 207936.0, 'sementes/mudas': 478252.8, 'agua': 1039680.0,
+     'nitrogenio': 2079360.0, 'fosforo': 1039680.0, 'potassio': 1663488.0})
+plantacoes.append(
+    {'nome': 'Plantação milho 3', 'cultura': 'milho', 'area': 622521.0, 'sementes/mudas': 1431798.2999999998,
+     'agua': 3112605.0, 'nitrogenio': 6225210.0, 'fosforo': 3112605.0, 'potassio': 4980168.0})
+
+main()
